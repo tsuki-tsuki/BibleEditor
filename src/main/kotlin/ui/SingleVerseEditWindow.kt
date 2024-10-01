bec4dcd221675746ad7a68c162dda74ba264fdc3
Jose <gdysay@gmail.com>
2024-10-02 00:54:54 +0900

    Cleanup design for edit window
    Fix clutter, allow keep dialog as controllable

--- src/main/kotlin/ui/SingleVerseEditWindow.kt
+++ src/main/kotlin/ui/SingleVerseEditWindow.kt
@@ -1,13 +1,13 @@
 package ui
 
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.OutlinedTextField
-import androidx.compose.material.Text
-import androidx.compose.material.TextButton
+import androidx.compose.material.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.semantics.Role
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.DialogWindow
 import entity.bible.Reference
 
@@ -24,7 +24,7 @@ fun SingleVerseEditWindow(
         resizable = false,
     ) {
         MaterialTheme {
-            Column(verticalArrangement = Arrangement.SpaceBetween, modifier = Modifier.fillMaxSize()) {
+            Column(verticalArrangement = Arrangement.SpaceBetween, modifier = Modifier.fillMaxSize().padding(10.dp)) {
                 var verseContent by remember(reference) { mutableStateOf(reference.verse.text) }
                 OutlinedTextField(
                     verseContent,
@@ -33,9 +33,25 @@ fun SingleVerseEditWindow(
                     modifier = Modifier.fillMaxWidth().weight(1f),
                 )
 
-                Row(modifier = Modifier.align(Alignment.End)) {
+                var keepOpen by remember { mutableStateOf(false) }
+                Row(
+                    horizontalArrangement = Arrangement.spacedBy(5.dp),
+                    verticalAlignment = Alignment.CenterVertically,
+                    modifier = Modifier.padding(top = 5.dp).clickable(role = Role.Checkbox) { keepOpen = !keepOpen },
+                ) {
+                    Checkbox(keepOpen, onCheckedChange = null)
+                    Text("Keep dialog open after save")
+                }
+                Row(
+                    horizontalArrangement = Arrangement.spacedBy(5.dp),
+                    verticalAlignment = Alignment.CenterVertically,
+                    modifier = Modifier.align(Alignment.End)
+                ) {
                     TextButton(
-                        onClick = { onSave(verseContent) },
+                        onClick = {
+                            onSave(verseContent)
+                            if (!keepOpen) onClose()
+                        },
                         enabled = reference.isValid && verseContent != reference.verse.text,
                     ) { Text("Save") }
                     TextButton(onClose) { Text("Cancel") }
